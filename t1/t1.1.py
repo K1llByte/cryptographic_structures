28f1c4daf21d1df735cc39bc05ac957660413766
KillByte <killbyte.prototype@gmail.com>
2021-03-28 00:44:36 +0000

    Added variable to switch key exchange methods

--- t1/t1.1.py
+++ t1/t1.1.py
@@ -13,6 +13,12 @@ from cryptography.hazmat.primitives.asymmetric import dh, dsa, ec
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.serialization import Encoding, ParameterFormat, PublicFormat
 
+DH_AND_DSA = 0
+ECDH_AND_ECDSA = 1
+# THIS VARIABLE DEFINES WHICH METHOD WILL BE
+# USED FOR KEY EXCHAGE
+key_exchange_method = ECDH_AND_ECDSA
+
 class Logger:
     def __init__(self,name):
         self.thread_name = name
@@ -59,13 +65,13 @@ def decrypt(nounce_ciphertext_mac,key):
 
 # Diffie-Hellman Key Exchange
 def emitter_key_exchange(channel,logger):
-    global emitter_dsa_private_key
-    global receiver_dsa_public_key
+    global emitter_private_key
+    global receiver_public_key
     # Emiter generates some DH parameters and sends to Receiver
     parameters = dh.generate_parameters(generator=2, key_size=1024)
     logger.log("Generated parameters")
 
-    signature = emitter_dsa_private_key.sign(
+    signature = emitter_private_key.sign(
         parameters.parameter_bytes(Encoding.DER,ParameterFormat.PKCS3),
         hashes.SHA256()
     )
@@ -78,7 +84,7 @@ def emitter_key_exchange(channel,logger):
     logger.log("Generated private and public key")
         
     # Send public_key to other peer
-    signature = emitter_dsa_private_key.sign(
+    signature = emitter_private_key.sign(
         public_key.public_bytes(Encoding.DER,PublicFormat.SubjectPublicKeyInfo),
         hashes.SHA256()
     )
@@ -87,7 +93,7 @@ def emitter_key_exchange(channel,logger):
     # Receive other peer's public_key
     peer_public_key, signature = channel.r2e.get()
     try:
-        receiver_dsa_public_key.verify(
+        receiver_public_key.verify(
             signature,
             peer_public_key.public_bytes(Encoding.DER,PublicFormat.SubjectPublicKeyInfo),
             hashes.SHA256()
@@ -108,8 +114,8 @@ def emitter_key_exchange(channel,logger):
 
 # Elliptic Curve Key Exchange
 def emitter_ec_key_exchange(channel,logger):
-    global emitter_dsa_private_key
-    global receiver_dsa_public_key
+    global emitter_private_key
+    global receiver_public_key
 
     # Entities generate both private and public key
     private_key = ec.generate_private_key(ec.SECP384R1())
@@ -117,19 +123,19 @@ def emitter_ec_key_exchange(channel,logger):
     logger.log("Generated private and public key")
 
     # Send public_key to other peer
-    signature = emitter_dsa_private_key.sign(
+    signature = emitter_private_key.sign(
         public_key.public_bytes(Encoding.DER,PublicFormat.SubjectPublicKeyInfo),
-        hashes.SHA256()
+        ec.ECDSA(hashes.SHA256())
     )
     channel.e2r.put((public_key,signature))
 
     # Receive other peer's public_key
     peer_public_key, signature = channel.r2e.get()
     try:
-        receiver_dsa_public_key.verify(
+        receiver_public_key.verify(
             signature,
             peer_public_key.public_bytes(Encoding.DER,PublicFormat.SubjectPublicKeyInfo),
-            hashes.SHA256()
+            ec.ECDSA(hashes.SHA256())
         )
     except InvalidSignature:
         logger.log("Invalid signature for Peer Public Key")
@@ -151,8 +157,10 @@ def emitter(channel):
     logger = Logger("Emitter")
     logger.log("Logger initialized")
 
-    key = emitter_ec_key_exchange(channel,logger)
-    
+    if key_exchange_method == DH_AND_DSA:
+        key = emitter_key_exchange(channel,logger)
+    if key_exchange_method == ECDH_AND_ECDSA:
+        key = emitter_ec_key_exchange(channel,logger) 
     logger.log(key)
 
     msg = b"Hello World!"
@@ -164,13 +172,13 @@ def emitter(channel):
 
 # Diffie-Hellman Key Exchange
 def receiver_key_exchange(channel,logger):
-    global receiver_dsa_private_key
-    global emitter_dsa_public_key
+    global receiver_private_key
+    global emitter_public_key
 
     # Get Emitter DH parameters
     parameters, signature = channel.e2r.get()
     try:
-        emitter_dsa_public_key.verify(
+        emitter_public_key.verify(
             signature,
             parameters.parameter_bytes(Encoding.DER,ParameterFormat.PKCS3),
             hashes.SHA256()
@@ -185,7 +193,7 @@ def receiver_key_exchange(channel,logger):
     logger.log("Generated private and public key")
 
     # Send public_key to other peer
-    signature = receiver_dsa_private_key.sign(
+    signature = receiver_private_key.sign(
         public_key.public_bytes(Encoding.DER,PublicFormat.SubjectPublicKeyInfo),
         hashes.SHA256()
     )
@@ -194,7 +202,7 @@ def receiver_key_exchange(channel,logger):
     # Receive other peer's public_key
     peer_public_key, signature = channel.e2r.get()
     try:
-        emitter_dsa_public_key.verify(
+        emitter_public_key.verify(
             signature,
             peer_public_key.public_bytes(Encoding.DER,PublicFormat.SubjectPublicKeyInfo),
             hashes.SHA256()
@@ -216,8 +224,8 @@ def receiver_key_exchange(channel,logger):
 
 # Elliptic Curve Key Exchange
 def receiver_ec_key_exchange(channel,logger):
-    global receiver_dsa_private_key
-    global emitter_dsa_public_key
+    global receiver_private_key
+    global emitter_public_key
 
     # Entities generate both private and public key
     private_key = ec.generate_private_key(ec.SECP384R1())
@@ -225,19 +233,19 @@ def receiver_ec_key_exchange(channel,logger):
     logger.log("Generated private and public key")
 
     # Send public_key to other peer
-    signature = receiver_dsa_private_key.sign(
+    signature = receiver_private_key.sign(
         public_key.public_bytes(Encoding.DER,PublicFormat.SubjectPublicKeyInfo),
-        hashes.SHA256()
+        ec.ECDSA(hashes.SHA256())
     )
     channel.r2e.put((public_key,signature))
 
     # Receive other peer's public_key
     peer_public_key, signature = channel.e2r.get()
     try:
-        emitter_dsa_public_key.verify(
+        emitter_public_key.verify(
             signature,
             peer_public_key.public_bytes(Encoding.DER,PublicFormat.SubjectPublicKeyInfo),
-            hashes.SHA256()
+            ec.ECDSA(hashes.SHA256())
         )
     except InvalidSignature:
         logger.log("Invalid signature for Peer Public Key")
@@ -259,8 +267,11 @@ def receiver(channel):
     logger = Logger("Receiver")
     logger.log("Logger initialized")
 
-    key = receiver_ec_key_exchange(channel,logger)
-
+    if key_exchange_method == DH_AND_DSA:
+        key = receiver_key_exchange(channel,logger)
+    elif key_exchange_method == ECDH_AND_ECDSA:
+        key = receiver_ec_key_exchange(channel,logger)
+    
     logger.log(key)
 
     nounce_ct = channel.e2r.get()
@@ -277,11 +288,15 @@ class Channel:
         self.r2e = Queue(5)
 
 
-emitter_dsa_private_key = dsa.generate_private_key(key_size=1024)
-emitter_dsa_public_key = emitter_dsa_private_key.public_key()
+if key_exchange_method == DH_AND_DSA:
+    emitter_private_key = dsa.generate_private_key(key_size=1024)
+    receiver_private_key = dsa.generate_private_key(key_size=1024)
+elif key_exchange_method == ECDH_AND_ECDSA:
+    emitter_private_key = ec.generate_private_key(ec.SECP384R1())
+    receiver_private_key = ec.generate_private_key(ec.SECP384R1()) #
 
-receiver_dsa_private_key = dsa.generate_private_key(key_size=1024)
-receiver_dsa_public_key = receiver_dsa_private_key.public_key()
+emitter_public_key = emitter_private_key.public_key()
+receiver_public_key = receiver_private_key.public_key()
 
 channel = Channel()
 
